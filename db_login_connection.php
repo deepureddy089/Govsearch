b92abbcd2f2ec902d7c7f904868ce9e77c446582
deepureddy089 <deepureddy089@gmail.com>
2024-12-22 20:11:41 +0930

    deploy test 3

--- db_login_connection.php
+++ db_login_connection.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "in-mum-cpl28"; // Database host
+$servername = "localhost:3306"; // Database host
 $username = "asse6007_admin"; // Database username (or your custom DB username)
 $password = "123456"; // Database password (or your custom DB password)
 $dbname = "asse6007_gov_schemes		"; // The database name for login functionality
