224c27bf85c19ead35e2bca580287a50933188eb
deepureddy089 <deepureddy089@gmail.com>
2024-12-24 20:31:57 +0930

    credentials updated

--- db_login_connection.php
+++ db_login_connection.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost"; // Database host
+$servername = "in-mum-cpl28"; // Database host
 $username = "asse6007_admin"; // Database username (or your custom DB username)
-$password = "123456"; // Database password (or your custom DB password)
+$password = "00000000"; // Database password (or your custom DB password)
 $dbname = "asse6007_gov_schemes"; // The database name for login functionality
 
 // Create a connection to the admin login database
