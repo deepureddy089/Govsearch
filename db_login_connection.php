84055c96b36180bd519f731906621ed8363f9df1
deepureddy089 <deepureddy089@gmail.com>
2024-12-24 21:00:45 +0930

    update

--- db_login_connection.php
+++ db_login_connection.php
@@ -5,7 +5,7 @@ $password = "00000000"; // Database password (or your custom DB password)
 $dbname = "asse6007_gov_schemes"; // The database name for login functionality
 
 // Create a connection to the admin login database
-$conn_login = new MySQLi($servername, $username, $password, $dbname);
+$conn_login = new Mysqli($servername, $username, $password, $dbname);
 
 if ($conn_login->connect_error) {
     die("Connection failed: " . $conn_login->connect_error);  // This will show any error in the connection
