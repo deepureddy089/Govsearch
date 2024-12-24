4c10d790136b378f7aca9426047cb861559eda02
deepureddy089 <deepureddy089@gmail.com>
2024-12-24 20:00:29 +0930

    mysqli -> mySQLi

--- db_login_connection.php
+++ db_login_connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "loclhost"; // Database host
+$servername = "localhost"; // Database host
 $username = "asse6007_admin"; // Database username (or your custom DB username)
 $password = "123456"; // Database password (or your custom DB password)
 $dbname = "asse6007_gov_schemes"; // The database name for login functionality
 
 // Create a connection to the admin login database
-$conn_login = new mysqli($servername, $username, $password, $dbname);
+$conn_login = new MySQLi($servername, $username, $password, $dbname);
 
 if ($conn_login->connect_error) {
     die("Connection failed: " . $conn_login->connect_error);  // This will show any error in the connection
