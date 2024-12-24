957d6462dba80d61f52c597182f57aa5f0edf55d
deepureddy089 <deepureddy089@gmail.com>
2024-12-24 19:00:39 +0930

    web test

--- search-form.php
+++ search-form.php
@@ -1,9 +1,10 @@
 <?php
 // Include database connection file
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "government_schemes";
+$servername = "localhost:3306"; // Database host
+$username = "asse6007_admin"; // Database username (or your custom DB username)
+$password = "123456"; // Database password (or your custom DB password)
+$dbname = "asse6007_gov_schemes"; // The database name for login functionality
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
